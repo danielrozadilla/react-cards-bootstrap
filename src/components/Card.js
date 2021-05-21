0d0628444508d5dc454bcc4d8417407778973007
drozadilla <danielrozadilla@gmail.com>
2021-05-21 16:06:50 -0300

    Las cards se generan a partir de un array de datos

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import imagen1 from "../assets/imagen1.jpg"
 
-function Card(title) {
+
+function Card({title, imageSrc, url}) {
   return (
     <div className="card text-center bg-dark ">
-        <img src={imagen1} alt="" />
+        <img src={imageSrc} alt="" />
         <div className="card-body text-light">
-            <h4 className="card-title">My title</h4>
+            <h4 className="card-title">{title}</h4>
             <p className="card-text text-secondary">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita nobis ullam asperiores commodi quidem? Obcaecati reiciendis dolore aspernatur porro quas! Magni obcaecati nihil ex maxime voluptatum dolores debitis laudantium. Officiis.</p>
-            <a href="#" className="btn btn-outline-secondary rounded-0">Go to this website</a>
+            <a href={url} target="_blank" className="btn btn-outline-secondary rounded-0">Go to this website</a>
         </div>         
     </div>
   );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,41 @@
 import React from 'react'
 import Card from './Card'
 
+import imagen1 from "../assets/imagen1.jpg"
+import imagen2 from "../assets/imagen2.jpg"
+import imagen3 from "../assets/imagen3.jpg"
+
+const cards=[
+    {
+        id:1,
+        title:'Lechuza',
+        image:imagen1,
+        url:'https://facebook.com'
+    },
+    {
+        id:2,
+        title:'Libelula',
+        image:imagen2,
+        url:'https://youtube.com'
+    },
+    {
+        id:3,
+        title:'Mariposa',
+        image:imagen3,
+        url:'https://google.com'
+    }
+];
+
 function Cards() {
   return (
     <div className="container d-flex justify-content-center h-100 align-items-center">
         <div className="row">
-            <div className="col-md-4">
-                <Card/>
-            </div>
-            <div className="col-md-4">
-                <Card/>
-            </div>
-            <div className="col-md-4">
-                <Card/>
-            </div>
+            {cards.map(card =>
+                <div className="col-md-4">
+                    <Card title={card.title} imageSrc={card.image} url={card.url}/>
+                </div> 
+            )}
+                      
         </div>        
     </div>
   )
